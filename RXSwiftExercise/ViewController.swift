03b5e30027a895251ccca8d127296bb405b6bb84
Ben LIU <arkilis@gmail.com>
2023-03-07 06:00:51 +1000

    Functional programming 1

--- RXSwiftExercise/ViewController.swift
+++ RXSwiftExercise/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var emailErrorInfo: UILabel!
+  
+  @IBOutlet weak var mobileTextField: UITextField!
+  @IBOutlet weak var mobileErrorInfo: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    
+    emailTextField.addTarget(self, action: #selector(emailTextFieldDidChange(_:)), for: .editingChanged)
+    
+    mobileTextField.addTarget(self, action: #selector(mobileTextFieldDidChange(_:)), for: .editingChanged)
+    
   }
-
-
+  
+  @objc func emailTextFieldDidChange(_ textField: UITextField) {
+    guard let email = textField.text else {
+      return
+    }
+    if email.contains("!") {
+      emailErrorInfo.text = "Invalid character"
+    } else {
+      emailErrorInfo.text = ""
+    }
+  }
+  
+  @objc func mobileTextFieldDidChange(_ textField: UITextField) {
+    guard let email = textField.text else {
+      return
+    }
+    if email.contains("@") {
+      mobileErrorInfo.text = "Invalid character"
+    } else {
+      mobileErrorInfo.text = ""
+    }
+  }
 }
 
